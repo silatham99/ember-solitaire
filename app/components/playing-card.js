4b97b63237fc2fb6a322b1e5cf6423cf4f0a0775
Scott Latham <silatham99@gmail.com>
2016-01-11 03:59:02 -0800

    Update card image logic to work with heroku

--- app/components/playing-card.js
+++ app/components/playing-card.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
 	classNameBindings: ['movable:movable', 'stackable:stackable', ':playing-card'],
-	attributeBindings: ['style', 'draggable'],
+	attributeBindings: ['draggable'],
 
 	draggable: Ember.computed('card.visible', function() {
 		return this.get('card.visible');
@@ -12,10 +12,6 @@ export default Ember.Component.extend({
 		var card = { id: this.get('card.id'), cardNumber: this.get('card.cardNumber'), suit: this.get('card.suit') };
     return event.dataTransfer.setData('card', JSON.stringify(card));
   },
-
-	style: Ember.computed('imageFilePath', function() {
-		return 'background-image: url("' + this.get('imageFilePath') + '"); background-size: 100%;';
-	}),
 
 	imageFilePath: function() {
 		if (this.get('card.visible')) {
